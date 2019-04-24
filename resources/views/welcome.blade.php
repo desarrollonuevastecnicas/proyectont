0dc8875bc76fb42ec18648218725a3f7e1a761de
Esteban Aristizabal <esteban.aristizabal@crifa.com>
2019-04-24 00:07:57 -0500

    email auth done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel ssssss
                 </div>
 
                 <div class="links">
